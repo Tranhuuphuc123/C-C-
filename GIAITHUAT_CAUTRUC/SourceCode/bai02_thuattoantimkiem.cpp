8cd9737ba2b694ebbd2c14cefee611bde484c3ff
Tranhuuphuc123 <Cantho1995ct@gmail.com>
2023-11-07 11:36:13 +0700

    luu thay doi 07/11/2023

--- GIAITHUAT_CAUTRUC/SourceCode/bai02_thuattoantimkiem.cpp
+++ GIAITHUAT_CAUTRUC/SourceCode/bai02_thuattoantimkiem.cpp
@@ -1,11 +1,11 @@
 // cùng tiềm hiểu về thuật toán tiềm kiếm thông qua bài toán sau
 /*
- >>Bài toán tiềm kiếm tuần tự<<
+ >>Bài toán tiềm kiếm tuần tự (Linear Search)<<
   => 
 */
 #include<iostream>
 using namespace std;
 
 int main(){
-
+   
 }
